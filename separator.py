40fa1c4fe7117950e93072afea6e1fab79f2fb1d
Gui-coder-alpha <guilhermesamt64@gmail.com>
2025-11-17 20:02:51 -0400

    separate_problems changes

--- separator.py
+++ separator.py
@@ -13,26 +13,32 @@ class Organize_files:
             self.read_lines = files_in_lines.readlines()
 
 
-    def transformation_split(self):
-        with open(self.file_csv, "r") as files_in_split:
-            read_str = files_in_split.read()
-            self.read_split = read_str.split()
 
     def separate_problems(self):
-        problems = {'Fail': 0,'fail':0,'error':0,'CRITICAL':0,'critical':0,'denied':0,
-                    'unauthorized':0,'failed':0,'Failed':0}
-        for word in self.read_split:
-            if word in problems:
-                problems[word] += 1
+        problems = {'fail':0,'error':0,'critical':0,'denied':0,
+                    'unauthorized':0,'failed':0}
+        for line in self.read_lines:
+            line_sec = line.lower()
+            for word in problems.keys():
+                if word in line_sec:
+                    problems[word] += 1
         print(problems)
     
     
     def ALL_TASKS(self):
         self.transformation_readlines()
-        self.transformation_split()
         self.separate_problems()
    
        
 
 FUNCTION_CLASS = Organize_files(path_storage)
 FUNCTION_CLASS.ALL_TASKS()
+
+
+
+
+
+   # def transformation_split(self):
+     #   with open(self.file_csv, "r") as files_in_split:
+      #      read_str = files_in_split.read()
+       #     self.read_split = read_str.split()
