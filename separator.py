8c17798b4944444d855b4081a39f3d033b1e4392
Gui-coder-alpha <guilhermesamt64@gmail.com>
2025-11-21 10:41:10 -0400

    Separator.py 1.0

--- separator.py
+++ separator.py
@@ -12,10 +12,6 @@ class Organize_files:
         with open(self.file_csv, "r") as files_in_lines:
             self.read_lines = files_in_lines.readlines()
 
-        with open(self.file_csv, "r") as files_in_split:
-            self.splitting_file = files_in_split.read()
-            self.true_split_file = self.splitting_file.split()
-
     def separate_problems(self):
         self.problems = {'fail':0,'error':0,'critical':0,'denied':0,
                     'unauthorized':0,'failed':0}
@@ -34,37 +30,41 @@ class Organize_files:
                     self.determined_problems[paragraph] += 1
 
     def determined_directory(self):
-        print("/////////////////////////////////////////")
         directory_path = "/proc/sys"
-        armazened_directories = []
-        for root, dirs, files in self.true_split_file:
-                for root, dirs, files in os.walk(directory_path):
-                    armazened_directories.append(root)
-        for path in armazened_directories:
-            print(path)
+        self.armazened_directories = {}
+        for root, dirs, files in os.walk(directory_path, directory_path):
+            self.armazened_directories[root] = 0
+        for line in self.read_lines:
+            true_line = line.lower()
+            for paragraph in self.armazened_directories:
+                if paragraph in true_line:
+                   self.armazened_directories[paragraph] += 1
+        for values_equals_zero in list(self.armazened_directories.keys()):
+            if self.armazened_directories[values_equals_zero] == 0:
+                del self.armazened_directories[values_equals_zero]
 
-        print("/////////////////////////////////////////")
-        
 
     def final_relatory(self):
         print('------------------------relatory------------------------')
         print(f"Observe a seguir o relatório de log:")
-        print(f"    fail:{self.problems['fail']}")
-        print(f"    error:{self.problems['error']}")
-        print(f"    failed:{self.problems['failed']}")
-        print(f"    unauthorized:{self.problems['unauthorized']}")
-        print(f"    critical:{self.problems['critical']}")
-        print(f"    denied:{self.problems['denied']}")
+        print(f"    -> fail:{self.problems['fail']}")
+        print(f"    -> error:{self.problems['error']}")
+        print(f"    -> failed:{self.problems['failed']}")
+        print(f"    -> unauthorized:{self.problems['unauthorized']}")
+        print(f"    -> critical:{self.problems['critical']}")
+        print(f"    -> denied:{self.problems['denied']}")
         print("Determined problems here:")
-        print(f"    Misc dxg:{self.determined_problems['misc dxg']}")
-        print(f"    Permission denied:{self.determined_problems['permission denied']}")
+        print(f"    -> Misc dxg:{self.determined_problems['misc dxg']}")
+        print(f"    -> Permission denied:{self.determined_problems['permission denied']}")
+        print(f"Access denied in directories:")
+        print(f"    ->{self.armazened_directories}")
         print('------------------------final------------------------')
 
     def ALL_TASKS(self):
-        self.determined_directory()
         self.transformation_readlines()
         self.determined_errors()
         self.separate_problems()
+        self.determined_directory()
         self.final_relatory()     
 
 FUNCTION_CLASS = Organize_files(path_storage)
